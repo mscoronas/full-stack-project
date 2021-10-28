dbde14cd36fa39cd9a7e45f16104c1c5f2b0aa30
Merviiiin <mervincoronas219@gmail.com>
2021-10-28 20:38:31 +0800

    updated index

--- TestProject/urls.py
+++ TestProject/urls.py
@@ -21,7 +21,7 @@ from movies import views as movies_views
 from courses.views import CoursesView, CreateCourseView
 
 urlpatterns = [
-    path('index/', myapp_views.index, name='index'),
+    path('', myapp_views.index, name='index'),
     path('my_name/', myapp_views.my_name, name='my_name'),
     path('movies/list/', movies_views.list, name='movies_list'),
     path('movies/detail/<int:movie_id>/', movies_views.detail, name='movie_detail'),
